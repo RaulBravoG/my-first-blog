8a6da2e9eac05a38d75f6862312c6f8f74ed3f3d
RaulBravoG <raulbravogarcia07@gmail.com>
2019-03-13 23:54:55 +0100

    my-first-blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!tr67g1#3pees-ij=2x3^%yil_ti#6*#8cnw+*m68(qh98+khb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '. pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'raulbravo.pythonanywhere.com']
 
 
 # Application definition
